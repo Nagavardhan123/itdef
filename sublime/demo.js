e54ca0b85ba8bc0c61ccc042f9005ac5aa134806
Nagavardhan123 <nagavardhanr11@gmail.com>
2024-01-17 12:36:35 +0530

    adding today's work

---
+++ sublime/demo.js
@@ -0,0 +1,24 @@
+// let x = `naga`
+// let y = `${x} is a good boy`
+// console.log(y)
+
+// let a = 20
+// console.log(a)
+
+// let b = `gova`
+// c = b[0].toUpperCase()
+// console.log(`you are a good boy ${c}ova`)
+
+// let data = `naga is a good boy`
+// console.log(data.substr(2,4))
+// console.log(data.substring(4,2))
+// let bb = data.split('good')
+// console.log(bb[1])
+// // console.log(bb)
+
+
+let a = 'naga'
+// console.log(a.indexOf('n'))
+// console.log(a.lastIndexOf('a'))
+console.log(a.replace('g','n'))
+console.log(a)
