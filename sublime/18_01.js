f41e8c1c013ffdf4416c51e974eca3814e8ebd25
Nagavardhan123 <nagavardhanr11@gmail.com>
2024-01-18 10:05:42 +0530

    work done on 18th jan

---
+++ sublime/18_01.js
@@ -0,0 +1,27 @@
+// let a = "naga is a good boy"
+// let part_of_a = a.slice(2,5)
+// console.log(part_of_a)
+
+// console.log(a)
+
+// console.log(part_of_a.repeat(2))
+// console.log(a.startsWith('Naga'))
+
+// let a = "naga"
+// let b = "reddy"
+// console.log(a + " " + b)
+
+// let a = ['a002','a108','a092']
+// console.log(a.sort())
+
+// let a = 'usn20'
+// console.log(a.padStart(8,'0'))
+// let a = "My name is P C Naga Vardhan Reddy"
+// console.log(a.search(/x/gi))
+
+// let  b = 'boy Boy BOY BOy'
+// console.log(b.match(/boy/gi))
+
+let c = "naga is a good boy1"
+console.log(c.match(/\w+\d/gi))
+
