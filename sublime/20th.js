6ae3b87d2aa7684af206cb152fd4d6f5f32730d7
Nagavardhan123 <nagavardhanr11@gmail.com>
2024-01-20 10:15:06 +0530

    20th work

---
+++ sublime/20th.js
@@ -0,0 +1,42 @@
+// // a = '100'
+// // b = '99'
+// // console.log(a===b)
+
+// // a = 12 + '20' + 1 + 20
+// // console.log(a)
+
+
+// // a = 12 + 1 + 20 + '20'
+// // console.log(a)
+
+// // a = 100
+// // // b = 'AAA'
+// // console.log(null == undefined)
+// // console.log(1 == true)
+// // console.log(-1 == true)
+// // console.log(0 == false)
+// // console.log(0 == '')
+// // console.log(NaN == NaN)  //false
+
+// // console.log(2 ** 'abc')   // not a number NaN
+
+// // a = ['Naga', 'gova', "RX", 'praneeth']
+// // console.log(a.sort())
+
+
+// let a = 1
+// let b = 2
+// let c = 3
+// if (a > b && a > c) {
+// 	console.log('the greatest number is:',a)
+// }
+// else if (b > a && b > c) {console.log('the greatest number is:',b)} else {console.log('the greatest number is:',c)}
+
+
+// let a = 1
+// console.log(`the value is ${a}`)
+
+
+let a = 1
+a == 2 ? console.log('the value is 2') : console.log('the value is 1')
+
