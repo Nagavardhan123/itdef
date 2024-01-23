9b4964ff70fd6635c00f2e47252f53fc8a2cb35c
Nagavardhan123 <nagavardhanr11@gmail.com>
2024-01-23 10:11:34 +0530

    23 jan work

---
+++ nagajavascripts/23jann.js
@@ -0,0 +1,23 @@
+// git stash and git stash pop
+//    used to checkout from one branch to another branch temporarily.. without commiting any changes....
+
+
+ // let a = Number(1)
+ // console.log(a)  
+ // let b = Array('1')
+ // console.log(b) 
+ // let c = []
+ // console.log(c) 
+ // let d = '123'.split("")
+ // console.log(d) 
+ // let e = Array()
+ // console.log(e) 
+ // 
+// let na = 'naga'
+// let a = [na,'lh','rx']
+// na = 'mana'
+// console.log(na)
+
+let a = ['1','2','3','4']
+a[16] = 'naga'
+console.log(a)
