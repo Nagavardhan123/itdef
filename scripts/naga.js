48435d6574bb7035e120dfda1b4788deced8c073
Nagavardhan123 <nagavardhanr11@gmail.com>
2024-01-24 19:02:02 +0530

    24th jan work

---
+++ scripts/naga.js
@@ -0,0 +1,8 @@
+// a = ['1','2','3','4','5','6','7','8','9'];
+// a.shift()
+// console.log(a)
+// a.splice(0,2,'1');
+// console.log(a.join(" "))
+// console.log(a)
+let a = 'abba'
+console.log(a.split('').reverse().join(''))
