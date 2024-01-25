0afde8adc826880d7fe38073ea0ef32786d3691f
Nagavardhan123 <nagavardhanr11@gmail.com>
2024-01-25 10:16:37 +0530

    work done on 25 jan

---
+++ scripts/whileloop.js
@@ -0,0 +1,24 @@
+// a = 1;
+// while (a==1){
+// 	console.log(a)
+// 	a--;
+// }
+// console.log(`came out of while loop`)
+
+// a = 5;
+// while (true){
+// 	console.log('my name is nagavardhan');
+// 	a--;
+// 	if (!a){
+// 		break;
+// 	}
+// }
+
+
+
+
+a = `nagaardhan reddy`
+function print(value){
+	console.log(value);
+}
+print(a)
