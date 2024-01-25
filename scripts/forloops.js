8b12b0fb3d170e1710ea38c6dfe731db7c1968d9
Nagavardhan123 <nagavardhanr11@gmail.com>
2024-01-25 21:37:00 +0530

    after class work

---
+++ scripts/forloops.js
@@ -0,0 +1,144 @@
+// write a 2 table using for loop
+
+
+// let a = 2;
+// for (i=1;i<=10;i++)
+// {
+// 	let b = a * i;
+// 	let c = b.toString().padStart(2,`0`)
+// 	console.log(`${a} X ${i.toString().padStart(2,`0`)} = ${c}`)
+// }
+
+// let a = 'naga'
+// // b = ['']
+// // for (var j = length(a); j >=0; i--) {
+// // 	b.push(a[j]);
+// // }
+// console.log(length(a))
+
+// let x = [1, 3454, 655, 24, 7, 8, 9999]
+// var b = x[0];
+// var c = x[1];
+// for (let i = 2; i < x.length; i++)
+// {
+// 	if (x[i] > b){
+// 		b = x[i];
+// 		if (x[i] < c) {
+// 			c = x[i];
+// 		console.log(c)
+// 	}
+// }	
+// }
+// // console.log(c)
+
+// const myArr = [[1,2],[3,4],[5,6]];
+// const newArr = myArr.flat();
+// console.log(newArr)
+
+// const months = ["Jan", "Feb", "Mar", "Apr"];
+// const spliced = months.toSpliced(0, 1);
+
+// console.log(months)
+// console.log(spliced)
+
+// let a = ["naga","gova","lh","rx"];
+// a.splice(1,0,2)
+// console.log(a)
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// // fruits.splice(0);
+// console.log(typeof(fruits))
+
+
+// let a = [1,2,3,4]
+// // b = a.indexOf(2)
+// // a.splice(b,1,0)
+// // console.log(a)
+// function funn(arr)
+// {
+// 	var b = arr[0];
+// for (var i = 1; i < arr.length; i++) {
+// 	if (arr[i] > b) {
+// 		b = arr[i];
+// 	}
+// }
+// }
+// funn(a)
+// inde  = a.indexOf(b)
+// a.splice(inde,1,0)
+// console.log(a)
+// console.log(b)
+// funn(a)
+// console.log(b)
+
+
+
+
+
+// function funn(arr)
+// {
+// 	var b = arr[0];
+// for (var i = 1; i < arr.length; i++) {
+// 	if (arr[i] > b) {
+// 		b = arr[i];
+// 	}
+// }
+// }
+
+
+
+
+
+
+
+// let a = [9,8,7,6]
+// var b = a[0];
+// for (let i = 1; i < a.length; i++) {
+// 	if (a[i] > b) {
+// 		b = a[i];
+// 	}
+// }
+// inde = a.indexOf(b)
+// a[inde] = 0
+// b = a[0];
+// for (let i = 1; i < a.length; i++) {
+// 	if (a[i] > b) {
+// 		b = a[i];
+// 	}
+// }
+// console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
