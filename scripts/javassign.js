ff9e22ee6e37126c75aa7707be50f8519530973e
Nagavardhan123 <nagavardhanr11@gmail.com>
2024-02-04 19:53:15 +0530

    add on's to javaass folder

--- scripts/javassign.js
+++ scripts/javassign.js
@@ -51,18 +51,41 @@
 
  // Write code using switch statement to show traffic signal light information
 
-let a ='orange'
+// let a ='orange'
 
-switch(a){
-case 'red':
-	console.log("it is red light");
-	break;
-case 'orange':
-	console.log("it is orange light")
-	break;
-case 'green':
-	console.log("it is green light")
-	break;
-default:
-	console.log("please enter one among 'red','orange' and 'green'")
-}
+// switch(a){
+// case 'red':
+// 	console.log("it is red light");
+// 	break;
+// case 'orange':
+// 	console.log("it is orange light")
+// 	break;
+// case 'green':
+// 	console.log("it is green light")
+// 	break;
+// default:
+// 	console.log("please enter one among 'red','orange' and 'green'")
+// }
+
+
+// conversion from snake to camel
+
+// let a ='NAga_vardhan_pc'
+// b = a.replace(/([a-z])_([a-z])/g,`$1$2`).split("")
+// console.log(b)
+// for (let i =0; i<b.length; i++){
+// 	if (i == 0){
+// 		b[i] = b[i].toLowerCase()
+// 	}
+// 	else{
+// 	b[i] = b[i][0].toUpperCase() + b[i].substr(1)
+// }
+// }
+// b = a.replace(/([a-z])_([a-z])/g,`$1$2`)   --> NAgavardhanpc-->['N','A']
+// // console.log(b.join(""))
+
+
+let a = prompt("enter a value: ")
+console.log(a)
+
+
