3dea62ae341c3eb3910b33df08fec3e719ecc6e3
Nagavardhan123 <nagavardhanr11@gmail.com>
2024-02-05 10:13:57 +0530

    feb 5th work

---
+++ scripts/5thfeb.js
@@ -0,0 +1,8 @@
+// let a = [1,2,3,4,5]
+// let out = a.every(el => el >=3)
+// console.log(out)
+// let out1 = a.some(el => el >=3)
+// console.log(out1)
+
+let [,b,...z] = [1,2,3,4,5,6,7]
+console.log(z)
